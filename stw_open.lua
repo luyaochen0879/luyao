0e6ffc5c25d7dc407614ead132fb0b4bb52d8ba6
luyaochen0879 <42428561+luyaochen0879@users.noreply.github.com>
2018-08-16 16:46:39 +0800

    Update stw_open.lua

--- stw_open.lua
+++ stw_open.lua
@@ -1,7 +1,7 @@
 wrk.method = "POST"
 wrk.headers["Content-Type"] = "application/json"
 
-wrk.headers["Cookie"] = "SPC_EC=ofOsmCNdvlccj9hWlfyZljvXcYGd8+UuSor6mWfEOSzb0cqsAkTXlZ1ADrSflpVXikOE9p96dQ6eBOZjbtJyhGiO6BI+4RFVdQNAf6fmMzkaUBGsob5gIrkTC8irqPmCMomeJJTMMrZwt5SpBmx5cw=="
+wrk.headers["Cookie"] = "SPC_EC=GzsXacOF7nrtsEvZkx95MBn69ldodO2VzfC8wR8kEk/Vk8HkKYijQuJO8chHIgry1vU1kEG2Bs7aRLlNMoAIvV85aF9nIiM/dBAghsTj1Z7Yk1RQmYkquM3n10bDuyheXCbvZo+aOJeQYTcCZxnkPA=="
 
 request = function()
     token = "XJ1NZrPiemUEFx7r0U498fPO0wqc6IaYN1GFYmSr7lntS7bcR66yvtXTZ+eJRYz9UlGcOtshdsInkkIFvVZ06Q=="
