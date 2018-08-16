a432beb8d1b9b7ccfffbb442673ec321def438fa
luyaochen0879 <42428561+luyaochen0879@users.noreply.github.com>
2018-08-16 17:13:16 +0800

    Update stw_open.lua

--- stw_open.lua
+++ stw_open.lua
@@ -6,6 +6,6 @@ wrk.headers["Cookie"] = "SPC_EC=GzsXacOF7nrtsEvZkx95MBn69ldodO2VzfC8wR8kEk/Vk8Hk
 request = function()
     token = "XJ1NZrPiemUEFx7r0U498fPO0wqc6IaYN1GFYmSr7lntS7bcR66yvtXTZ+eJRYz9UlGcOtshdsInkkIFvVZ06Q=="
     path = "/api/v1/luckydraw/104/open/"
-    body = "{\"times\":\""..token.."\"}"
+    body = "{\"token\":\""..token.."\"}"
     return wrk.format(nil,path,nil,body)
 end
