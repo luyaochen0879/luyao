921d2bafd5bc7d1e974bb10fab3ab3d5e89d5f85
luyaochen0879 <42428561+luyaochen0879@users.noreply.github.com>
2018-08-17 14:45:54 +0800

    Delete rain_surprise.lua

--- rain_surprise.lua
+++
@@ -1,7 +0,0 @@
-wrk.method = "POST"
-wrk.headers["Content-Type"] = "application/x-www-form-urlencoded"
-wrk.headers["Cookie"] = "SPC_EC=ArbEJcTjE0RnrekCvQ1iT9wZUKpHVtliwbiboiJ/mVCNR++3OUsP2EMkIRsZbZjQpgg+xXNqnnp7SVmwdVTtY4ntdNdE0X+v6D0uxrMAlrblkOVImrH4cWogLhjBExtJoI5Ivj2qdCyNGJ8/pItjeg=="
-request = function()
-    path = "/gameabr/share/api/v1/15/107/settlement/surprise/"
-    return wrk.format(nil,path)
-end
