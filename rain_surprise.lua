2b8a3ad7b1311591ef4bc6c1f0d0b95542068dd3
luyaochen0879 <42428561+luyaochen0879@users.noreply.github.com>
2018-08-17 14:46:52 +0800

    Add files via upload

---
+++ rain_surprise.lua
@@ -0,0 +1,3 @@
+wrk.method = "POST"
+wrk.headers["Content-Type"] = "application/json"
+wrk.headers["Cookie"] = "SPC_EC=ArbEJcTjE0RnrekCvQ1iT9wZUKpHVtliwbiboiJ/mVCNR++3OUsP2EMkIRsZbZjQpgg+xXNqnnp7SVmwdVTtY4ntdNdE0X+v6D0uxrMAlrblkOVImrH4cWogLhjBExtJoI5Ivj2qdCyNGJ8/pItjeg=="
